65e4aba001be55b1bdf7da50046d7b0534d9bfae
vianepw <viane.213040103@mail.unpas.ac.id>
2023-12-18 23:12:52 +0700

    footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -135,8 +135,35 @@
         </a>
         @endforeach
       </div>
+
+
+      <!-- Footer Section -->
+      @include('footer')
+      <div class="w-full bg-develobe-900 h-[320px] text-white flex flex-row pt-12">
+        <div class="w-6/12 pl-28 flex flex-col">
+          <span class="font-quicksand text-4xl font-bold">CINEMATION</span>
+          <span class="font-inter text-lg mt-4">Kami menyediakan review film dan TV Series<br>yang bisa jadi acuan tontonanmu<br>#BetterMovieBetterLife</span>
+          <span class="font-inter text-lg mt-4">&copy; 2023 CINEMATION</span>
+        </div>
+        
+        <div class="w-3/12 flex flex-col">
+          <span class="font-inter font-bold text-lg">Website</span>
+          <a href="/" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">Home</a>
+          <a href="/movies" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">Movies</a>
+          <a href="/tv-shows" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">TV Shows</a>
+        </div>
+        
+        <div class="w-3/12 flex flex-col">
+          <span class="font-inter font-bold text-lg">Social</span>
+          <a href="https://www.instagram.com/develobe.education/" target="_blank" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">Instagram</a>
+          <a href="https://discord.gg/mCqP3WBXgB" target="_blank" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">Discord</a>
+        </div>
+      </div>
+
+
     </div>
 
+    
 
     <script language="javascript">
       // banner pertama
