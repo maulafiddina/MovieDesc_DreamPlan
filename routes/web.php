7c463270d9efb0875d259975ae1509dd24a57d3f
Daffa Arkantaqqy <isupply@Daffa-Arkantaqqy.local>
2023-12-18 23:02:27 +0700

    daffa 1

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,9 @@
 use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 Route::get('/', [MovieController::class, 'index']);
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'processLogin']);
