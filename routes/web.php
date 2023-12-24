62f8a3110ad936797b7c948d29ac8d324c357cab
Daffa Arkantaqqy <isupply@Daffa-Arkantaqqy.local>
2023-12-24 13:51:12 +0700

    login page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\LoginController;
+
 Route::get('/', [MovieController::class, 'index']);
 Route::get('/movies', [MovieController::class, 'movies']);
+
+Route::get('/login', [LoginController::class,'index']);
+Route::post('/login/login', [LoginController::class,'login']);
