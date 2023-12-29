42d68435ef8d4b56a14968e68d6544d9b271ba66
maulafiddina <96758441+maulafiddina@users.noreply.github.com>
2023-12-29 15:15:15 +0700

    dashboard (#15)

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,18 @@
-<h1>INI DASHBOARD</h1>
+<html>
+  <head>
+    <title>CINEMATION | Better Movie Better Life</title>
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <div class="w-full h-auto min-h-screen flex flex-col">
+      <!-- Home Section -->
+      @include('header')
+
+      
+
+      <!-- Footer Section -->
+      @include('footer')
+    </div>
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+  </body>
+</html>
