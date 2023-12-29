79232aaa01cac5669d87aa73287cffcc80b0e1f0
maula fiddina <dzikra.213040081@mail.unpas.ac.id>
2023-12-29 15:07:47 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,13 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', [MovieController::class, 'index']);
 Route::get('/movies', [MovieController::class, 'movies']);
 Route::get('/tv-shows', [MovieController::class, 'tvShows']);
 Route::get('/search', [MovieController::class, 'search']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
 
 
 Route::get('/login', [LoginController::class,'index'])->name('login');
@@ -24,10 +26,9 @@ Route::post('/login/login', [LoginController::class,'login']);
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
 
-
-Route::get('/dashboard', function(){
-    return view('dashboard.index');
-})->middleware('auth');
+// Route::get('/dashboard', function(){
+//     return view('dashboard.index');
+// })->middleware('auth');
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'processLogin']);
